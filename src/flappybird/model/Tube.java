dc9fd935b55a92ba9bf507a62cb38fe854080e58
Uygar KAYA <uygar.kaya@ozu.edu.tr>
2021-02-14 17:21:04 +0300

    Tube is created

---
+++ src/flappybird/model/Tube.java
@@ -0,0 +1,32 @@
+package flappybird.model;
+
+import flappybird.model.proxy.ProxyImage;
+import java.awt.*;
+import java.awt.image.ImageObserver;
+
+public class Tube extends GameObject {
+
+    private ProxyImage proxyImage;
+
+    public Tube(int x, int y) {
+        super(x, y);
+        if (proxyImage == null)
+            proxyImage = new ProxyImage("/assets/TubeBody.png");
+        this.image = proxyImage.loadImage().getImage();
+        this.width = image.getWidth(null);
+        this.height = image.getHeight(null);
+    }
+
+    @Override
+    public void tick() {
+        this.x -= dx;
+    }
+
+    @Override
+    public void render(Graphics2D g, ImageObserver obs) {
+        g.drawImage(image, x, y, obs);
+    }
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+}
