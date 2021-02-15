c9aafbd212c1da24188199890bef13897646e9db
Uygar KAYA <uygar.kaya@ozu.edu.tr>
2021-02-15 14:36:48 +0300

    Container is created

---
+++ src/flappybird/view/Container.java
@@ -0,0 +1,34 @@
+package flappybird.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Container extends JPanel {
+
+    private static CardLayout cardLayout;
+    private static Game gamePanel;
+    private static ContainerLR containerLRPanel;
+    private static Entrance entrancePanel;
+
+    public Container() {
+        cardLayout = new CardLayout();
+        this.setLayout(cardLayout);
+
+        containerLRPanel = new ContainerLR();
+        entrancePanel = new Entrance();
+        gamePanel = new Game();
+
+        this.add(containerLRPanel, "1");
+        this.add(entrancePanel, "2");
+        this.add(gamePanel, "3");
+
+        cardLayout.show(this, "1");
+    }
+
+    public static CardLayout getCardLayout(){
+        return cardLayout;
+    }
+    public static Game getGamePanel(){
+        return gamePanel;
+    }
+}
